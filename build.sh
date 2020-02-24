931d30f4605d04f2a85b6646b3af9f2313e43033
Matthew Rutter <rutter.matt@ymail.com>
2020-02-23 23:51:19 -0500

    added build file and changed directory structure for gradle

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-javac -d ./build -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar ./src/java/*.java ./test/java/EdgeConnectorTest.java
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:build org.junit.runner.JUnitCore EdgeConnectorTest
-java -cp build RunEdgeConvert
